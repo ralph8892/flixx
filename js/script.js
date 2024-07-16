6ce59c4591f8b52a06f0f2931f7d26ff4b8adafe
ralph8892 <brufalralph@gmail.com>
2024-07-16 15:55:23 +0800

    Display popular tv shows functionality

--- js/script.js
+++ js/script.js
@@ -35,16 +35,61 @@ async function displayPopularMovies () {
   });
 }
 
+async function displayPopularShows () {
+  const { results } = await fetchAPIData('tv/popular');
+  
+  results.forEach(show => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+                    src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                    class="card-img-top"
+                    alt="${show.name}"
+                  />` 
+                  : `<img
+                      src="../images/no-image.jpg"
+                      class="card-img-top"
+                      alt="${show.name}"
+                    />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Pilot Release: ${show.first_air_date}</small>
+            </p>
+          </div>
+    `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchAPIData (endpoint) {
   const API_KEY = '72cb460a303c46d2738994fddbaea2ca';
   const API_URL = 'https://api.themoviedb.org/3/';
 
+  showSpinner();
+
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner () {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner () {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 // Hightlight Active Link
@@ -62,11 +107,10 @@ function init () {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      // console.log('Home');
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
