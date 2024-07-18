c85b4e8df2f5f7cd5af4b250105a61fd76bdf663
ralph8892 <brufalralph@gmail.com>
2024-07-18 12:12:26 +0800

    Display search results functionality implemented

--- js/script.js
+++ js/script.js
@@ -225,11 +225,50 @@ async function search () {
 
   if (global.search.term !== '' && global.search.term !== null) {
     // Make request and display results
-    const results = await searchAPIData();
-    console.log(results);
+    const { results, total_pages, page } = await searchAPIData();
+    
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value = '';
   } else {
     showAlert('Please enter a search term');
   }
+}
+
+function displaySearchResults (results) {
+  results.forEach(result => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+                    src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+                    class="card-img-top"
+                    alt="${global.search.type === 'movie' ? result.title : result.name}"
+                  />` 
+                  : `<img
+                      src="../images/no-image.jpg"
+                      class="card-img-top"
+                      alt="${global.search.type === 'movie' ? result.title : result.name}"
+                  />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>
+    `;
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 // Display Slider Movies
@@ -328,7 +367,7 @@ function highlightActiveLink () {
   })
 }
 
-function showAlert (message, className) {
+function showAlert (message, className = 'error') {
   const alertEl = document.createElement('div');
   alertEl.classList.add('alert', className);
   alertEl.appendChild(document.createTextNode(message));
